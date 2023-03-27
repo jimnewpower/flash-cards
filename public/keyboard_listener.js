f8dcffc32ddb00081d88ec52d1c0f53ef39a2706
Jim Newpower <jimnewpower@gmail.com>
2023-03-27 08:54:45 -0600

    Add keyboard listener.
    Now user can answer questions by pressing (1,2,3,4), and advance to next question by pressing <enter> or right-arrow key.

---
+++ public/keyboard_listener.js
@@ -0,0 +1,33 @@
+function checkKey(e) {
+    e = e || window.event;
+
+    console.log(e.keyCode);
+
+    switch (e.keyCode) {
+        case 49://keyboard 1
+        case 97://keypad 1
+            // 1
+            checkAnswer(0);
+            break;
+        case 50://keyboard 2
+        case 98://keypad 2
+            // 2
+            checkAnswer(1);
+            break;
+        case 51://keyboard 3
+        case 99://keypad 3
+            // 3
+            checkAnswer(2);
+            break;
+        case 52://keyboard 4
+        case 100://keypad 4
+            // 4
+            checkAnswer(3);
+            break;
+        case 13://enter
+        case 39://right arrow
+            // enter
+            nextQuestion();
+            break;
+    }
+}
