0a94a9cc47a5339fcc28f5f358ed6d2fa2c9d8f0
Jim Newpower <jimnewpower@gmail.com>
2023-04-08 16:43:24 -0600

    Add categories and titles

--- frontend/public/startup.js
+++ frontend/public/startup.js
@@ -1,5 +1,6 @@
 const baseUrl = 'http://localhost:9090';
 const apiBaseUrl = '/api/v1';
+const containerPrefix = 'category-';
 
 function buildURL(endpoint) {
     return baseUrl + apiBaseUrl + endpoint;
@@ -16,6 +17,10 @@ function startup() {
     document.getElementById("home-button").disabled = true;
 
     document.getElementById("topic").innerHTML = "Select Quiz Type";
+
+    document.getElementById("categories").setAttribute("style", "display: content;");
+    document.getElementById("titles").setAttribute("style", "display: content;");
+
     document.getElementById("multiple-choice-button").setAttribute("style", "display: content;");
     document.getElementById("flash-card-button").setAttribute("style", "display: content;");
     document.getElementById("all-flash-cards-button").setAttribute("style", "display: content;");
@@ -34,6 +39,17 @@ function startup() {
         })
         .then(data => {
             console.log(data);
+
+            let html = '';
+            data.categories.forEach(element => {
+                let category = element.toLowerCase().replace(/ /g, '-');
+                let id = containerPrefix + category;
+                console.log('id: ' + id);
+                html += `<div class="container" id="${id}">${element}</div>`;
+            });
+            html += '';
+            document.getElementById("categories").innerHTML = html;
+
             fetchTitles();
         })
         .catch(error => {
@@ -55,6 +71,16 @@ function fetchTitles() {
         })
         .then(data => {
             console.log(data);
+
+            data.titles.forEach(element => {
+                let category = element.category.toLowerCase().replace(/ /g, '-');
+                let id = containerPrefix + category;
+                console.log('id: ' + id);
+                let container = document.getElementById(id);
+                console.log(container);
+    
+                container.innerHTML += '<li><a href="#">' + element.title + '</a></li>';
+            });
         })
         .catch(error => {
             showError(error);
