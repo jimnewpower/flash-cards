c1e7e05c6981a1e324e6ea4ea6276fd1ecfdd8c8
Jim Newpower <jimnewpower@gmail.com>
2023-04-11 17:46:35 -0600

    Copy in appropriate Dockerfile

--- api/build_run.sh
+++ api/build_run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ./gradlew build
+rm ./Dockerfile
+cp ./Dockerfile_Standard ./Dockerfile
 docker build -t jimnewpower/flashapi .
 docker run --env-file ./docker.env -p 9090:9090 jimnewpower/flashapi
